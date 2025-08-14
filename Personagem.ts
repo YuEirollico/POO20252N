e73b9f387b8c2aca26597bd51c2316763d158b08
YURI EIRÓLICO ALMEIDA <01011680041@senacrs.edu.br>
2025-08-14 20:43:20 -0300

    Adiciona Personagem

---
+++ Personagem.ts
@@ -0,0 +1,13 @@
+export class Personagem{
+    nome: string = "";
+    class: string = "";
+    raca: string = "";
+    nivel: number = 0;
+    arma: string = "";
+    manaAtual: number = 0;
+    manaMaxima: number = 0;
+    vidaatual: number = 0;
+    vidamaxima: number = 0;
+    poderAtaque: number = 0;
+
+}
